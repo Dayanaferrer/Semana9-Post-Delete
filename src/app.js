30aa8a0c80195ef3cd6c39f42fd55f2948cd373e
dayanaferrer <dayanaferreira2@gmail.com>
2020-10-10 17:38:36 -0300

    commit final

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
 const routesLivros = require('./router/livrosRouter')
+const index = require("./routes/index")
 
+app.use(bodyParser.json());
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+  })
+
+app.use("/", index)
 app.use ('/', routesLivros)
 
 module.exports = app
--- src/router/livrosRouter.js
+++ src/router/livrosRouter.js
@@ -1,12 +1,39 @@
 const express = require ('express');
 const router = express.Router();
 const controller = require ('../controller/livrosController');
+const fs = require('fs');
+const { resourceUsage} = require('process');
 
+router.get ('/',controller.getAllLivros);
 router.get ('/livros',controller.getAllLivros);
 router.delete('/:id', controller.deletarLivro);
 
+const getById = (req, res) => {
+    const id = req.params.id;
+  
+    res.status(200).send(tarefas.find((controller) => controller.id == id));
+  };
+
+
+  const postLivros = (req, res) => {
+    console.log(req.body);
+    const { id, nome, genero, autora, editora, numeroDePaginas, dataDePublicacao } = req.body;
+    tarefas.push({ id, nome, genero, autora, editora, numeroDePaginas, dataDePublicacao });
+  
+    fs.writeFile("./src/model/livros.json", JSON.stringify(controller), 'utf8', function(err) {
+      if (err) {
+        return res.status(424).send({ message: err });
+      }
+      console.log("-----> Arquivo atualizado!!!");
+    });
+  
+    res.status(201).send(tarefas);
+  };
+  
 router.get('/colaboradoras', controller.getAllColaboradoras);
 router.delete('/colaboradoras/:id',controller.deletarColaboradoras);
+
+router.post('/',controler.postLivros)
 
 router.get('/livros/:genero', controller.getLivrosGenero);
 router.get('/idade/:id', controller.getAgeByID)
