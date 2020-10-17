0e5bacd4fe4f5b5a4aa1497489641b7c039bbf1f
dayanaferrer <dayanaferreira2@gmail.com>
2020-10-17 13:54:38 -0300

    controller colaboradoras

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 8083
 
 app.listen(port, () => {
     console.log(`-----> Servidor rodando na porta ----> ${port} `)
-})
+});
--- src/controller/colaboradorasController.js
+++ src/controller/colaboradorasController.js
@@ -1,7 +1,10 @@
 const colaboladoras = require( '../model/colaboradoras.json' );
 const fs = require ('fs');
+const { resourceUsage } = require('process');
+
 
 const getColaboradoras = (req, res) => {
+    console.log (req.url);
     res.status(200).send(colaboladoras);
 }
 
@@ -41,9 +44,69 @@ const deletarColaboradoras = (req, res) => {
     res.status(200).send(colaboladoras);
 }
 
+const putColaboradoras = (req, res) => {
+    try {
+        const id = req.params.id;
+        const colaboradorasASerModificada = colaboladoras.find((colaboladoras) => colaboladoras.id == id);
+        
+        console.log(colaboradorasASerModificada);
+
+        const colaboradorasAtualizadas = req.body;
+        console.log (colaboradorasAtualizadas);
+
+        const index = funcionarios.indexOf(colaboradorasASerModificada);
+        console.log(index);
+
+        colaboladoras.splice(index, 1, colaboradorasAtualizadas);
+        console.log(colaboladoras)
+
+        fs.writeFile('./src/model/colaboradoras.json', JSON.stringify(colaboladoras), 'utf8', function (err){
+            if (err){
+                return res.status(424).send({message: err});
+            }
+            console.log('Arquivo atualizado com sucesso!');
+        }
+
+        )}
+
+        res.status(200).send(colaboladoras)
+} catch (err) {
+    return res.status(424).send ({message: err})
+}
+
+
+const patchColaboradoras = (req,res) => {
+    const id = req.params.id;
+    const atualizacao = req.body;
+    console.log(colaboladoras);
+
+    try{
+        const colaboradorasASerModificada= colaboladoras.find((colaboladoras)=> colaboladoras.id == id);
+        console.log(Object.keys(colaboradorasASerModificada))
+
+        Object.keys(atualizacao).forEach ((chave)=> {
+            colaboradorasASerModificada[chave] = atualizacao[chave];
+        });
+        fs.writeFile('./src/model/colaboradoras.json', 'utf8', function (err){
+            if (err) {
+                return res.status(424).send({message: err});
+            }
+            console.log('Arquivo atualizado')
+        }
+        )
+        return res.status(200).send(colaboladoras);
+    
+    }catch (err){
+        return res.status(424).send({message: err})
+    }
+}
+
+
 module.exports = {
     getColaboradoras,
     postColaboradoras,
     deletarColaboradoras,
-    getColaboradorasById
+    getColaboradorasById,
+    putColaboradoras,
+    patchColaboradoras
 }
